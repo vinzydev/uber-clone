8e234d500aaec7281a6a181ad3b068874039c23c
vinzydev <vinzydev@gmail.com>
2024-08-02 15:20:55 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "bucket0097" # Replace with your actual S3 bucket name
+    bucket = "rider-terraform" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
